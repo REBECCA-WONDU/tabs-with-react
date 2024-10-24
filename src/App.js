200d369f0152b9240938ceeff08b62471323606b
REBECCA-WONDU <ribkawondu@gmail.com>
2024-10-24 13:11:04 -0700

    tabs with react and css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Tabs from "./components/Tabs";
+// import Navs from "./components/Navs";
+
+function App() {
+  return (
+    <div>
+      {/* <Navs /> */}
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navs.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+
+// export default function Nav() {
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-center">
+//         <ul className="nav-links">
+//           <li>
+//             <a>Tab 1</a>
+//           </li>
+//           <li>
+//             <a>Tab 2</a>
+//           </li>
+//           <li>
+//             <a>Tab 3</a>
+//           </li>
+//           <li>
+//             <a>Tab 4</a>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
